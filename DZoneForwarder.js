6ad82ff9a751ed630e9e722b8561d73605f09d8a
Andrey Lipatkin <lipatkin@gmail.com>
2015-12-29 21:04:45 +0300

    Initial commit

---
+++ DZoneForwarder.js
@@ -0,0 +1,16 @@
+// DZoneForwarder
+// version 0.0.1 beta
+// 2015-12-29
+// Released under the MIT license
+// ==UserScript==
+// @name DZoneForwarder
+// @namespace https://github.com/Litee/DZoneForwarder	
+// @description GreaseMonkey script for automatically redirecting from DZone web page to the original article
+// @include https://dzone.com/links/*
+// @require https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// ==/UserScript==
+
+var link = $('.link-title > a').attr('href');
+if (link) {
+	window.location.href = link;
+}
